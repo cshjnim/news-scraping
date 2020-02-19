09174510388ea32c5ec470a6cc63a0c60e35d6bb
Hyejin Kim <cshjkim@gmail.com>
2020-02-18 19:47:25 -0800

    updating the server js resolved no connecting error

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var path = require("path")
+var bodyParser = require("body-parser")
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -10,6 +12,9 @@ var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
 
 var PORT = process.env.PORT || 8080;
 
@@ -26,10 +31,39 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+//need to set Handlebars
+var exphbs = require("express-handlebars")
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+  partialsDir: path.join(__dirname, "/views/index")
+}));
+app.set("view engine", "handlebars");
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/news-scraping", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
 
 // Routes
+
+////////////////////////ROUTES TO MAIN PAGE
+app.get("/", function (req, res) {
+  Article.find({ "saved": false }, function (error, data) {
+      var hbsObject = {
+          article: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+  });
+});
+
+app.get("/saved", function (req, res) {
+  Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
+      var hbsObject = {
+          article: articles
+      };
+      res.render("saved", hbsObject);
+  });
+});
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
@@ -39,23 +73,26 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("div.story-body").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
+      result.title = $(element)
+        .children("h2.headline")
         .text();
-      result.link = $(this)
-        .children("a")
+      result.link = $(element)
+        .find("a")
         .attr("href");
+      result.summary = $(element)
+        .find("p.summary")
+        .text();
 
       // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
+      Article.create(result)
+        .then(function(data) {
           // View the added result in the console
-          console.log(dbArticle);
+          console.log(data);
         })
         .catch(function(err) {
           // If an error occurred, log it
@@ -66,6 +103,20 @@ app.get("/scrape", function(req, res) {
     // Send a message to the client
     res.send("Scrape Complete");
   });
+});
+
+
+//////////ROUTE: CLEAR UNSAVED
+app.get('/clear', function(req, res) {
+  db.Article.remove({ saved: false}, function(err, doc) {
+      if (err) {
+          console.log(err);
+      } else {
+          console.log('removed');
+      }
+
+  });
+  res.redirect('/');
 });
 
 // Route for getting all Articles from the db
@@ -116,6 +167,80 @@ app.post("/articles/:id", function(req, res) {
       // If an error occurred, send it to the client
       res.json(err);
     });
+});
+
+//////////////////////////ROUTE TO DELETE
+app.post("/articles/delete/:id", function (req, res) {
+  //Anything not saved
+  Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": false, "notes": [] })
+
+      .exec(function (err, data) {
+          // Log any errors
+          if (err) {
+              console.log(err);
+          }
+          else {
+              res.send(data);
+          }
+      });
+});
+
+////////////////////////ROUTE FOR COMMENT
+app.post("/notes/save/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note({
+      body: req.body.text,
+      article: req.params.id
+  });
+  console.log(req.body)
+  // And save the new note the db
+  newNote.save(function (error, note) {
+
+      if (error) {
+          console.log(error);
+      }
+      else {
+          Article.findOneAndUpdate({ "_id": req.params.id }, { $push: { "notes": note } })
+              /////???EXEC VS THEN???
+              .exec(function (err) {
+
+                  if (err) {
+                      console.log(err);
+                      res.send(err);
+                  }
+                  else {
+                      res.send(note);
+                  }
+              });
+      }
+  });
+});
+
+/////////////////////////ROUTE TO DELTE A NOTE
+app.delete("/notes/delete/:note_id/:article_id", function (req, res) {
+  // Use the note id to find and delete it
+  Note.findOneAndRemove({ "_id": req.params.note_id }, function (err) {
+      // Log any errors
+      if (err) {
+          console.log(err);
+          res.send(err);
+      }
+      else {
+          Article.findOneAndUpdate({ "_id": req.params.article_id }, { $pull: { "notes": req.params.note_id } })
+              // Execute the above query
+              .exec(function (err) {
+                  // Log any errors
+                  if (err) {
+                      console.log(err);
+                      res.send(err);
+                  }
+                  else {
+                      // Or send the note to the browser
+                      res.send("Note Deleted");
+                  }
+              });
+      }
+  });
 });
 
 // Start the server
